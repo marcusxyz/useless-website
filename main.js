bb4ae891758bc22dfb2be8095bb92a022c0ebda0
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-08 16:01:27 +0100

    eventlistener added to range input

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const sentenceTag = document.querySelector(`input[type="text"]`);
+const sentenceTag = document.querySelector(`input[type='text']`);
 const outputTag = document.querySelector('textarea.output');
 const originalText = outputTag.value;
+const typesizeTag = document.querySelector(`input[name='typesize']`);
+const typesizeOutput = document.querySelector('span.typesize-output');
 
 // When the user types in sentenceTag, update the outputTag
 // List of KeyboardEvent https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
@@ -18,4 +20,12 @@ sentenceTag.addEventListener('keyup', () => {
 // Typing from textarea
 outputTag.addEventListener('keyup', () => {
   sentenceTag.value = outputTag.value;
+});
+
+// When I change my typesize slider, update the span text and change font size in outputTag
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+
+typesizeTag.addEventListener('input', () => {
+  typesizeOutput.innerHTML = typesizeTag.value + 'px'; // changing HTML sspan text
+  outputTag.style.fontSize = typesizeTag.value + 'px'; // changing CSS style
 });
