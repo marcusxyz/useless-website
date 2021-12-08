b5fe4ded78822fe4ef7374d90c0d8aa6955d4ce8
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-08 14:54:47 +0100

    added if statement to keyup eventlistener

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const sentenceTag = document.querySelector(`input[type="text"]`);
 const outputTag = document.querySelector('section.output');
+const originalText = outputTag.innerHTML;
 
 // When the user types in sentenceTag, update the outputTag
 // List of KeyboardEvent https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 
-sentenceTag.addEventListener('keyup', () => {
-  outputTag.innerHTML = sentenceTag.value;
+const displayUpdate = sentenceTag.addEventListener('keyup', () => {
+  if (sentenceTag.value) {
+    outputTag.innerHTML = sentenceTag.value;
+  } else {
+    // If there is no value add the original text
+    outputTag.innerHTML = originalText;
+  }
 });
