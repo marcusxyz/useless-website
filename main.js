0442803cd9ed38edb7a4916f6c3dfbfe1cb08484
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-09 13:07:29 +0100

    added option to change bg colors

--- main.js
+++ main.js
@@ -11,10 +11,15 @@ const typesizeOutput = document.querySelector('span.typesize-output');
 const lineheightTag = document.querySelector(`input[name='lineheight']`);
 const lineheightOutput = document.querySelector('span.lineheight-output');
 
+// For checkbox
+const italicTag = document.querySelector(`input[name='italic']`);
+
+// For colors
+const colorTags = document.querySelectorAll('div.colors div');
+
 // When the user types in sentenceTag, update the outputTag
 // List of KeyboardEvent https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 
-// Typing from input
 sentenceTag.addEventListener('keyup', () => {
   if (sentenceTag.value) {
     outputTag.value = sentenceTag.value;
@@ -33,11 +38,38 @@ outputTag.addEventListener('keyup', () => {
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
 
 typesizeTag.addEventListener('input', () => {
-  typesizeOutput.innerHTML = typesizeTag.value + 'px'; // changing HTML sspan text
+  typesizeOutput.innerHTML = typesizeTag.value + 'px'; // changing HTML span text
   outputTag.style.fontSize = typesizeTag.value + 'px'; // changing CSS style
 });
 
 lineheightTag.addEventListener('input', () => {
   lineheightOutput.innerHTML = lineheightTag.value;
   outputTag.style.lineHeight = lineheightTag.value; // outputTag once again, because this is where you change lineheight in CSS
+});
+
+// When I click my checkbox, make the outputTag italic
+italicTag.addEventListener('change', () => {
+  if (italicTag.checked) {
+    console.log('Checkbox is checked..');
+    outputTag.style.fontStyle = 'italic';
+  } else {
+    console.log('Checkbox is not checked..');
+    outputTag.style.fontStyle = 'normal';
+  }
+});
+
+// When I pick a specific color, update background color of outputTag
+colorTags.forEach((colorTag) => {
+  colorTag.addEventListener('click', () => {
+    outputTag.style.backgroundColor = colorTag.style.backgroundColor;
+    outputTag.style.color = colorTag.style.color;
+
+    //Go throught all tags and remove selected
+    colorTags.forEach((notSelected) => {
+      notSelected.classList.remove('selected');
+    });
+
+    // Add selected class
+    colorTag.classList.add('selected');
+  });
 });
